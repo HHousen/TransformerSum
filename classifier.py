9f80e924368a0cfbdf0302a974479a89ccd5c585
Hayden Housen <hayden@haydenhousen.com>
2020-04-28 23:46:22 -0400

    Fix and comment TransformerEncoderClassifier forward()

--- classifier.py
+++ classifier.py
@@ -75,6 +75,8 @@ class TransformerEncoderClassifier(nn.Module):
                                      sigmoid will automatically be created. (default: nn.Sequential(linear, sigmoid))
         """        
         super(TransformerEncoderClassifier, self).__init__()
+        self.nhead = nhead
+
         encoder_layer = nn.TransformerEncoderLayer(d_model, nhead, dim_feedforward=dim_feedforward, dropout=dropout)
         layer_norm = nn.LayerNorm(d_model)
         self.encoder = nn.TransformerEncoder(encoder_layer, num_layers, norm=layer_norm)
@@ -87,7 +89,32 @@ class TransformerEncoderClassifier(nn.Module):
             self.reduction = nn.Sequential(linear, sigmoid)
     
     def forward(self, x, mask):
-        x = self.encoder(x, mask)
+        # add dimension in the middle
+        attn_mask = mask.unsqueeze(1)
+        # expand the middle dimension to the same size as the last dimension (the number of sentences/source length)
+        # Example with batch size 2: There are two masks since there are two sequences in the batch. Each mask
+        # is a list of booleans for each sentence vector. The below line expands each of these lists by duplicating
+        # them until they are each as long as the number of sentences. Now instead of a list of booleans, each mask
+        # is a matrix where each row is identical. This effectively masks tokens where the entire column is False.
+        # Slight Explanation (for 2D not 3D): https://discuss.pytorch.org/t/how-to-add-padding-mask-to-nn-transformerencoder-module/63390/3
+        # Detailed Explanation for Beginners: https://github.com/bentrevett/pytorch-seq2seq/blob/master/4%20-%20Packed%20Padded%20Sequences%2C%20Masking%2C%20Inference%20and%20BLEU.ipynb
+        # PyTorch MultiheadAttention Docs: https://pytorch.org/docs/stable/nn.html#torch.nn.MultiheadAttention.forward
+        attn_mask = attn_mask.expand(-1, attn_mask.size(2), -1)
+        # repeat the mask for each attention head
+        attn_mask = attn_mask.repeat(self.nhead, 1, 1)
+        # attn_mask is shape (batch size*num_heads, target sequence length, source sequence length)
+        # set all the 0's (False) to negative infinity and the 1's (True) to 0.0 because the attn_mask is additive
+        attn_mask = attn_mask.float().masked_fill(attn_mask == 0, float('-inf')).masked_fill(attn_mask == 1, float(0.0))
+
+        x = x.transpose(0, 1)
+        # x is shape (source sequence length, batch size, feature number)
+
+        x = self.encoder(x, mask=attn_mask)
+        # x is still shape (source sequence length, batch size, feature number)
         x = self.reduction(x)
+        # x is shape (source sequence length, batch size, 1)
+        x = x.transpose(0, 1).squeeze()
+        # x is shape (batch size, source sequence length)
+        # mask is shape (batch size, source sequence length)
         sent_scores = x.squeeze(-1) * mask.float()
         return sent_scores
